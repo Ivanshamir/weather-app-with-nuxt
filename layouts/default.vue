6afb6d6ccfc1732c6153037ac6acb5f49871b05b
Ivanshamir <shamirimtiaz.ice@gmail.com>
2020-07-19 00:56:58 +0600

    project with nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-main>
+        <v-toolbar color="indigo" dark>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+          <n-link to="/">
+            <v-toolbar-title>Home</v-toolbar-title>
+          </n-link>
+
+          <v-spacer></v-spacer>
+
+          <v-btn text >
+            <n-link to="/weather-app">Weather App</n-link>
+          </v-btn>
+        </v-toolbar>
+        <transition>
+          <nuxt />
+        </transition>
+    
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+a {
+  text-decoration: none;
+  color: white !important;
+}
+</style>
---
+++ pages/weather-app.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-container>
+          <h1 class="display-1 text-center">Weather App</h1>
+          <v-flex xs-12>
+              <v-card color="blue-grey darken-2" dark> 
+                <v-card-text>
+                    <v-layout justify-center>
+                        <v-flex xs-4 >
+                            <h4 class="text-center">Temperature</h4>
+                            <h1 class="display-1 text-center">
+                                {{ weather.name }}
+                                 <img :src="icon" alt="weather-icon">
+                            </h1>
+                            <p>
+                                <span class="display-1">{{ temp() }} &deg;C
+                                </span>
+                                <span class="caption ml-4">
+                                    {{weather.weather[0].description}}
+                                </span>
+                            </p>     
+                        </v-flex>
+                        <v-flex xs-4 >
+                            <h4 class="text-center">Wind & Pressure:</h4>
+                            <h3 class="headline">
+                                Wind: {{weather.wind.speed}} m/s ({{weather.wind.deg}} &deg;C)
+                            </h3>   
+                            <h3 class="headline mt-4">
+                                Humidity: {{weather.main.humidity}} %
+                            </h3>   
+                            <h3 class="headline mt-4">
+                                Pressure: {{weather.main.pressure}} hPa   
+                            </h3>   
+                        </v-flex>
+                        <v-flex xs-4 >
+                            <h4 class="text-center">Other:</h4>
+                            <h3 class="headline mt-4">
+                                Max Temperature: {{ Math.round(weather.main.temp_max - 273)}} &deg;C
+                            </h3>   
+                            <h3 class="headline mt-4">
+                                Min Temperature: {{ Math.round(weather.main.temp_min - 273)}} &deg;C
+                            </h3>   
+                        </v-flex>
+                    </v-layout>
+                </v-card-text>
+              </v-card>
+          </v-flex>
+          <v-flex xs12>
+              <v-form @submit.prevent="getWeatherInfo">        
+                <v-text-field v-model="city" label="Enter City Name" solo></v-text-field>
+              </v-form>
+          </v-flex>
+  </v-container>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            city: "london",
+            // weather: {}
+        }
+    },
+    asyncData ({ params, $axios }) {
+        return $axios.$get(`https://api.openweathermap.org/data/2.5/weather?q=london&appid=${process.env.weatherAppId}`)
+        .then((res) => {
+            return { weather: res }
+        })
+    },
+    computed:{
+        icon(){
+            return `https://openweathermap.org/img/wn/${this.weather.weather[0].icon}.png`
+        }
+    },
+    methods:{
+        getWeatherInfo(){
+            this.$axios.$get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${process.env.weatherAppId}`)
+                    .then(res => (this.weather = res));
+        },
+        temp(){
+            return Math.round(this.weather.main.temp - 273);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
